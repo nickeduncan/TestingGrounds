8ad6a3682f6565819fc91d06e09689a2164317cb
Nicholas Duncan <nickeduncan@gmail.com>
2018-01-22 15:05:28 -0500

    [TG-18] Got NextWaypointIndex in C++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,11 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "TestingGrounds.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-    UE_LOG(LogTemp, Warning, TEXT("COOL"));
+    auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+    auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    
+    UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
     return EBTNodeResult::Succeeded;
 }
