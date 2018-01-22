1b0380de1d3639c71c01b32dfb5c7f0bc5148a4b
Nicholas Duncan <nickeduncan@gmail.com>
2018-01-22 16:14:16 -0500

    [TG-21] Created patrol route in C++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,17 +3,22 @@
 #include "ChooseNextWaypoint.h"
 #include "TestingGrounds.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" // TODO remove coupling
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-    // TODO protect against empty patrol routes
+    // TODO protect against no patrol route component
     
-    // Get the patrol points
-    auto AIController = OwnerComp.GetAIOwner();
-    auto ControlledPawn = AIController->GetPawn();
-    auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-    auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+    // Get the patrol route
+    auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+    auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+    if(!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+    
+    // TODO protect against empty patrol routes
+    auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+    if(PatrolPoints.Num() == 0) {
+        UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points."));
+    }
     
     // Set next waypoint
     auto BlackboardComp = OwnerComp.GetBlackboardComponent();
--- Source/TestingGrounds/PatrollingGuard.h
+++ Source/TestingGrounds/PatrollingGuard.h
@@ -14,7 +14,7 @@ class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
-public: // remove or create GET
+public: 
     UPROPERTY (EditInstanceOnly, Category = "Patrol Route")
     TArray<AActor*> PatrolPointsCPP;
 	
